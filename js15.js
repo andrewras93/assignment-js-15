8a40210b9e92f39723bb44869fd1c9a8a4ae743a
andrewras93 <andrewras93@gmail.com>
2021-09-02 15:41:17 +0200

    Initial commit.

---
+++ js15.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let fahrenheit  = window.prompt('Enter fahrenheit temparture');
+let celsius     = 5/9*(fahrenheit-32);
+
+console.log(`Fahrenheit: ${fahrenheit}, Celsius ${celsius}`);
